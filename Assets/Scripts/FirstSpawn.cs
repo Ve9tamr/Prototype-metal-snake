d92db2a69154b20fceb3bcbf64aedb83a8acc1f0
Ve9tamr <smilekamch@mail.ru>
2022-06-24 12:27:29 +1200

    Tutorial

--- Assets/Scripts/FirstSpawn.cs
+++ Assets/Scripts/FirstSpawn.cs
@@ -6,14 +6,18 @@ public class FirstSpawn : MonoBehaviour
 {
     public GameObject SpawnMaster;
     private EnemySpawn makespawn;
+    public GameObject MainUI;
+    private MenuUIController MainUIControl;
 
     private void Awake()
     {
         makespawn = SpawnMaster.GetComponent<EnemySpawn>();
+        MainUIControl = MainUI.GetComponent<MenuUIController>();
     }
     public void MFSpawn()
     {
         makespawn.EncounterAchived();
         Destroy(gameObject);
+        MainUIControl.Tutorial3();
     }
 }
--- Assets/Scripts/MenuUIController.cs
+++ Assets/Scripts/MenuUIController.cs
@@ -8,7 +8,9 @@ public class MenuUIController : MonoBehaviour
 
     public static bool GameIsPaused;
     public GameObject PauseMenuUI;
-    public GameObject TutorialMenuUI;
+    public GameObject TutorialMenuUI1;
+    public GameObject TutorialMenuUI2;
+    public GameObject TutorialMenuUI3;
     public GameObject WinMenuUI;
     public GameObject LoseMenuUI;
 
@@ -17,7 +19,9 @@ public class MenuUIController : MonoBehaviour
         PauseMenuUI.SetActive(false);
         WinMenuUI.SetActive(false);
         LoseMenuUI.SetActive(false);
-        TutorialMenuUI.SetActive(true);
+        TutorialMenuUI2.SetActive(false);
+        TutorialMenuUI3.SetActive(false);
+        TutorialMenuUI1.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
@@ -25,9 +29,24 @@ public class MenuUIController : MonoBehaviour
     public void Resume()
     {
         PauseMenuUI.SetActive(false);
-        TutorialMenuUI.SetActive(false);
+        TutorialMenuUI1.SetActive(false);
+        TutorialMenuUI2.SetActive(false);
+        TutorialMenuUI3.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
+    }
+
+    public void Tutorial2()
+    {
+        TutorialMenuUI2.SetActive(true);
+        TutorialMenuUI1.SetActive(false);
+    }
+
+    public void Tutorial3()
+    {
+        TutorialMenuUI3.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
     }
 
     public void Pause()
